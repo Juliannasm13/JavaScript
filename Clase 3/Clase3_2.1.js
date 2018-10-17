241f97f00b174310a94d3621252e8e47cb13dddb
Julianna <juliannasm120@hotmail.com>
2018-10-17 11:57:03 -0430

    Agragada la clase 4

--- Clase 3/Clase3_2.1.js
+++ Clase 3/Clase3_2.1.js
@@ -2,6 +2,10 @@ var nombre="sin nombre";
 
 function leer_Datos(){
 	nombre=prompt("Nombre: ");
+	if (nombre==null || nombre=="")
+		alert("Debe escribir el nombre")
+	else
+		alert("El nombre es "+nombre)
 }
 function saludar(){
 	alert("El nombre es "+nombre)
